735f869390ee14abedefdb6bf052252fa16bc2d8
Jose A . <kaiten76@hotmail.com>
2026-02-03 23:20:43 +0100

    practica fundamentos js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,52 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+let presupuesto=0;
 // TODO: Variable global
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    let resultado=0;
+    if (valor >=0 ){
+        presupuesto=valor;
+        resultado=valor;
+    }
+    else{
+        alert("ERROR, dato no válido");
+        resultado=-1;
+    }
+
+    return resultado;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+   let texto= "Tu presupuesto actual es de " + presupuesto + " €"
+   return texto
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    if (valor < 0 ){
+        valor=0;
+    }
+    
+    let gasto = {
+        descripcion: descripcion,
+        valor: valor,
+
+        mostrarGasto: function() {
+        alert("Gasto correspondiente a "+ this.descripcion +" con valor " + this.valor +" €") 
+        },
+         actualizarDescripcion: function(descripcion) {
+        this.descripcion=descripcion;
+        },
+        actualizarValor: function(valor) {
+            if (valor >=0 ){
+            this.valor=valor; 
+            }
+        }
+
+    }
+    return gasto;
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
