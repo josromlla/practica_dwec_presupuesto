4a898e9b3fe294b98ee35884a13536daafcebd91
Jose <kaiten76@hotmail.com>
2026-02-07 02:07:08 +0100

    subida para eliminar el error del ejercicio js1 y parte del ejercicio 2

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,7 +3,7 @@
 // TODO: Variable global
 let presupuesto=0;
 let gastos= new Array();
-let idGasto;
+let idGasto=0;
 
 function actualizarPresupuesto(valor) {
     let resultado=0;
@@ -24,19 +24,28 @@ function mostrarPresupuesto() {
 }
 
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha ,...etiquetas ) {
     
-    if (valor < 0) {
-        this.valor = 0;
-    } else {
-        this.valor = valor;
+    if (valor>=0){
+         this.valor = valor;
+    }
+    else{
+        this.valor=0;
     }
     
     this.descripcion = descripcion;
+    this.idGasto=idGasto+1;
+    idGasto++;
 
-    this.fecha
-    this.etiquetas=new Array();
+    this.fecha=compruebaFecha(fecha);    
     
+    if (etiquetas == null){
+        this.etiquetas=new Array();
+    }
+    else{
+         this.etiquetas=etiquetas;
+    }
+
     this.mostrarGasto = function() {
         return("Gasto correspondiente a "+ this.descripcion +" con valor " + this.valor +" €") 
     };
@@ -49,6 +58,49 @@ function CrearGasto(descripcion, valor) {
         if (valor >= 0) {
             this.valor = valor;
         }
+    };
+    this.mostrarGastoCompleto = function(){
+       texto= "Gasto correspondiente a "+this.descripcion+" con valor "+"this.valor"+" €. \n"+
+       "Fecha: " +this.fecha.toLocaleString()+ "\n"+
+        "Etiquetas:"
+        
+       // Etiquetas:
+       // - ETIQUETA 1
+       // - ETIQUETA 2
+       // - ETIQUETA 3
+        
+        return texto;
+    };
+    this.actualizarFecha = function(fecha){
+       //intentamos convertir la fecha recibida en timestamp
+        let timestamp=Date.parse(fecha)
+        //condicion para comprobar si timestamp no es un número
+        if (!isNaN(timestamp)){
+            this.fecha=timestamp;
+        }        
+    };
+    this.anyadirEtiquetas = function(...etiquetas){
+         
+         for (let i=0; i< etiquetas.length; i++){
+            if (!this.etiquetas.includes(etiquetas[i])){
+                this.etiquetas.push(etiquetas[i]);
+            }            
+         }
+    };
+
+    this.borrarGasto = function(registro){
+        //buscamos el indice
+        let indice=-1;
+        for (i=0 ; i<gastos.length ; i++){
+            if ( gastos[i].idGasto == registro )
+                indice=i
+        }
+        
+        //eliminamos el registro con ese indice
+        if (indice !==-1 )
+            gastos.splice(indice,1);
+        else
+            console.log("el registro no se encuentra")
     };
 }
 
@@ -66,6 +118,18 @@ function calcularTotalGastos(){
 }
 function calcularBalance(){
 
+}
+
+function compruebaFecha(fecha){
+    //intentamos convertir la fecha recibida en timestamp
+    let timestamp=Date.parse(fecha)
+    //condicion para comprobar si timestamp no es un número
+    if (isNaN(timestamp)){
+        return fecha= new Date().getTime();
+    }
+    else{
+        return timestamp;
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
